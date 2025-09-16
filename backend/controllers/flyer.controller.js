9074e8a375a422432d0ec809c729e4eb4407b783
Ninetales-Publishings <147030239+saurabhkuriyal@users.noreply.github.com>
2025-09-16 23:42:01 +0530

    crating backend for flyer

--- backend/controllers/flyer.controller.js
+++ backend/controllers/flyer.controller.js
@@ -3,13 +3,13 @@ async function createFlyer(req,res) {
 
         console.log("reached here in flyer controller",req.body,req.file);
 
-        res.status(200).json({success:true,message: "Flyer created successfully"});
+        return res.status(200).json({success:true,message: "Flyer created successfully"});
         
         
     } catch (error) {
         console.log("flyer creation error",error );
         
-        res.status(500).json({success:false, message: "Internal Server Error"});
+        return res.status(500).json({success:false, message: "Internal Server Error"});
     }
 }
 
