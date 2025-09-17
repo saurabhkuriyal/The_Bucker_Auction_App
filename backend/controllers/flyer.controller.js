27a8f1bf0aaff9a9e1ef6fc3bd7ee71b289bddd4
Ninetales-Publishings <147030239+saurabhkuriyal@users.noreply.github.com>
2025-09-17 15:00:13 +0530

    adding cloudinary for getting images

--- backend/controllers/flyer.controller.js
+++ backend/controllers/flyer.controller.js
@@ -1,7 +1,23 @@
+//const cloudinary = require("cloudinary").v2;
+
 async function createFlyer(req,res) {
     try {
 
-        console.log("reached here in flyer controller",req.body,req.file);
+        console.log("reached here in flyer controller",req.body);
+        console.log("file details",req.file);
+
+        postImage = "";
+        if (req.file) {
+            cloudinary.config({
+                cloud_name: "deuofkrkf",
+                api_key:process.env.CLOUDINARY_API_KEY,
+                api_secret: "vrtWkACC1-Tra5I0WzJ6tIsstLw"
+            });
+
+            const result = await cloudinary.uploader.upload(req.file.path);
+            //console.log(result.secure_url, 'uploaded.secure_url');
+            postImage = result.secure_url;
+        }
 
         return res.status(200).json({success:true,message: "Flyer created successfully"});
         
