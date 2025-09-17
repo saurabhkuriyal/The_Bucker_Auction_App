8c18534a874c0f5aeb2de55ae61681a051e32c7a
Ninetales-Publishings <147030239+saurabhkuriyal@users.noreply.github.com>
2025-09-17 15:34:46 +0530

    done the flyer backend

--- backend/controllers/flyer.controller.js
+++ backend/controllers/flyer.controller.js
@@ -1,4 +1,5 @@
-//const cloudinary = require("cloudinary").v2;
+const cloudinary = require("cloudinary").v2;
+const Flyer = require("../models/flyer.model");
 
 async function createFlyer(req,res) {
     try {
@@ -6,7 +7,7 @@ async function createFlyer(req,res) {
         console.log("reached here in flyer controller",req.body);
         console.log("file details",req.file);
 
-        postImage = "";
+        flyerImage = "";
         if (req.file) {
             cloudinary.config({
                 cloud_name: "deuofkrkf",
@@ -15,10 +16,20 @@ async function createFlyer(req,res) {
             });
 
             const result = await cloudinary.uploader.upload(req.file.path);
-            //console.log(result.secure_url, 'uploaded.secure_url');
-            postImage = result.secure_url;
+            console.log(result.secure_url, 'uploaded.secure_url');
+            flyerImage = result.secure_url;
         }
 
+        const { title, description, } = req.body;
+
+        const newFlyer = new Flyer({
+            title,
+            description,
+            imageUrl: flyerImage,
+        });
+
+        console.log("new flyer details----",newFlyer);
+        
         return res.status(200).json({success:true,message: "Flyer created successfully"});
         
         
