ce045b791ea86fac97891fb3dc4a85de47367c50
Ninetales-Publishings <147030239+saurabhkuriyal@users.noreply.github.com>
2025-09-22 23:09:10 +0530

    removing console for production

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -6,7 +6,7 @@ async function registerUser(req,res) {
 
         const { username, email, password, phone } = req.body;
 
-        console.log("reacher here",req.body);
+       // console.log("reacher here",req.body);
 
         if(!username || !email || !password) {
             console.log("missing fields");
@@ -28,7 +28,7 @@ async function registerUser(req,res) {
         return res.status(201).json({success:true,data:newUser,message: "User registered successfully"});
         
     } catch (error) {
-        console.log("user registration error",error);
+        //console.log("user registration error",error);
 
         return res.status(500).json({success:false, message: "Internal Server Error"});
         
@@ -40,7 +40,7 @@ async function loginUser(req,res) {
     try {
         const { email, password } = req.body;
 
-        console.log("reacher here",req.body);
+        //console.log("reacher here",req.body);
         
 
         if(!email || !password) {
@@ -63,17 +63,18 @@ async function loginUser(req,res) {
         return res.status(200).json({success:true,data:user,message: "Login successful"});
 
     } catch (error) {
-        console.log("user login error",error);
+        //console.log("user login error",error);
         return res.status(500).json({success:false, message: "Internal Server Error"});
     }
 }
 
 async function getAllUser(req, res) {
     try {
+        //to get all users
         const users = await User.find();
         return res.status(200).json({ success: true, data: users, message: "Users fetched successfully" });
     } catch (error) {
-        console.log("get all users error", error);
+        //console.log("get all users error", error);
         return res.status(500).json({ success: false, message: "Internal Server Error" });
     }
 }
