dddeee81f7d2d9e2056da2380f2611327f0d904b
Ninetales-Publishings <147030239+saurabhkuriyal@users.noreply.github.com>
2025-09-25 23:38:17 +0530

    formating models and routes

--- backend/routes/user.route.js
+++ backend/routes/user.route.js
@@ -6,7 +6,9 @@ const userController = require('../controllers/user.controller');
 router.use(bodyParser.json());
 
 router.route("/register").post(userController.registerUser);
+
 router.route("/login").post(userController.loginUser);
+
 router.route("/getAllUser").get(userController.getAllUser);
 
 
