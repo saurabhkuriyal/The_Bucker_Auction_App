73f8e0a2dc437d908ffbce5c5acbcb2fe5f4dfcf
Ninetales-Publishings <147030239+saurabhkuriyal@users.noreply.github.com>
2025-10-04 23:05:35 +0530

    encapsulating Header component

--- frontend/src/components/custom/Header.jsx
+++ frontend/src/components/custom/Header.jsx
@@ -38,30 +38,31 @@ export default function Example() {
     const [mobileMenuOpen, setMobileMenuOpen] = useState(false)
 
     return (
-        <header className="bg-white my-1.5 pr-3">
-            <nav aria-label="Global" className=" flex max-w-7xl items-center justify-between pt-2 lg:px-8">
-                <div className="flex lg:flex-1">
-                    <a href="#" className="mx-auto">
-                        <span className="sr-only">Your Company</span>
-                        <img
-                            alt=""
-                            src="/logo.jpeg"
-                            className="h-27 w-auto"
-                        />
-                    </a>
-                </div>
-                <div className="flex lg:hidden mb-1">
-                    <button
-                        type="button"
-                        onClick={() => setMobileMenuOpen(true)}
-                        className="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-700"
-                    >
-                        <span className="sr-only">Open main menu</span>
-                        <Bars3Icon aria-hidden="true" className="size-6" />
-                    </button>
-                </div>
-                <PopoverGroup className="hidden lg:flex lg:gap-x-12 mb-6">
-                    {/* <Popover className="relative">
+        <>
+            <header className="bg-white my-1.5 pr-3">
+                <nav aria-label="Global" className=" flex max-w-7xl items-center justify-between pt-2 lg:px-8">
+                    <div className="flex lg:flex-1">
+                        <a href="#" className="mx-auto">
+                            <span className="sr-only">Your Company</span>
+                            <img
+                                alt=""
+                                src="/logo.jpeg"
+                                className="h-27 w-auto"
+                            />
+                        </a>
+                    </div>
+                    <div className="flex lg:hidden mb-1">
+                        <button
+                            type="button"
+                            onClick={() => setMobileMenuOpen(true)}
+                            className="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-700"
+                        >
+                            <span className="sr-only">Open main menu</span>
+                            <Bars3Icon aria-hidden="true" className="size-6" />
+                        </button>
+                    </div>
+                    <PopoverGroup className="hidden lg:flex lg:gap-x-12 mb-6">
+                        {/* <Popover className="relative">
                         <PopoverButton className="flex items-center gap-x-1 text-sm/6 font-semibold text-gray-900">
                             Home
                             <ChevronDownIcon aria-hidden="true" className="size-5 flex-none text-gray-400" />
@@ -105,72 +106,72 @@ export default function Example() {
                         </PopoverPanel>
                     </Popover> */}
 
-                    <Link href="/" className="text-sm/6 font-semibold text-gray-900">
-                        Home
-                    </Link>
-                    <Link href="/about" className="text-sm/6 font-semibold text-gray-900">
-                        About
-                    </Link>
+                        <Link href="/" className="text-sm/6 font-semibold text-gray-900">
+                            Home
+                        </Link>
+                        <Link href="/about" className="text-sm/6 font-semibold text-gray-900">
+                            About
+                        </Link>
 
-                    <Link href="/salering" className="text-sm/6 font-semibold text-gray-900">
-                        Sale Ring
-                    </Link>
-                    <Link href="/TermsandServices" className="text-sm/6 font-semibold text-gray-900">
-                        Terms and Services
-                    </Link>
-                    <Link href="/contact" className="text-sm/6 font-semibold text-gray-900">
-                        Contact
-                    </Link>
-                </PopoverGroup>
-                <div className="hidden lg:flex lg:flex-1 lg:justify-end">
-                    {/* <a href="#" className="text-sm/6 font-semibold text-gray-900">
+                        <Link href="/salering" className="text-sm/6 font-semibold text-gray-900">
+                            Sale Ring
+                        </Link>
+                        <Link href="/TermsandServices" className="text-sm/6 font-semibold text-gray-900">
+                            Terms and Services
+                        </Link>
+                        <Link href="/contact" className="text-sm/6 font-semibold text-gray-900">
+                            Contact
+                        </Link>
+                    </PopoverGroup>
+                    <div className="hidden lg:flex lg:flex-1 lg:justify-end">
+                        {/* <a href="#" className="text-sm/6 font-semibold text-gray-900">
                         <button className="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600">
                         Log in <span aria-hidden="true">&rarr;</span>
                         </button>
                     </a> */}
-                    <Link href="/register" className="text-sm/6 font-semibold mb-6 text-gray-900">
-                        <Button
-                            variant="outline"
-                            className="mx-1"
-                        >
-                            Register
-                        </Button>
-                    </Link>
-                    <Link href="/login" className="text-sm/6 font-semibold mb-6 text-gray-900">
-                        <Button
-                            variant="default"
-                            className="bg-[#426C6B] text-white hover:bg-[#325251]"
-                        >
-                            Log in <span aria-hidden="true">&rarr;</span>
-                        </Button>
-                    </Link>
-                </div>
-            </nav>
-            <Dialog open={mobileMenuOpen} onClose={setMobileMenuOpen} className="lg:hidden">
-                <div className="fixed inset-0 z-50" />
-                <DialogPanel className="fixed inset-y-0 right-0 z-50 w-full overflow-y-auto bg-white p-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
-                    <div className="flex items-center justify-between">
-                        <a href="#" className="-m-1.5 p-1.5">
-                            <span className="sr-only">Your Company</span>
-                            <img
-                                alt=""
-                                src="/logo.jpeg"
-                                className="h-25 w-auto"
-                            />
-                        </a>
-                        <button
-                            type="button"
-                            onClick={() => setMobileMenuOpen(false)}
-                            className="-m-2.5 rounded-md p-2.5 text-gray-700"
-                        >
-                            <span className="sr-only">Close menu</span>
-                            <XMarkIcon aria-hidden="true" className="size-6" />
-                        </button>
+                        <Link href="/register" className="text-sm/6 font-semibold mb-6 text-gray-900">
+                            <Button
+                                variant="outline"
+                                className="mx-1"
+                            >
+                                Register
+                            </Button>
+                        </Link>
+                        <Link href="/login" className="text-sm/6 font-semibold mb-6 text-gray-900">
+                            <Button
+                                variant="default"
+                                className="bg-[#426C6B] text-white hover:bg-[#325251]"
+                            >
+                                Log in <span aria-hidden="true">&rarr;</span>
+                            </Button>
+                        </Link>
                     </div>
-                    <div className="mt-6 flow-root">
-                        <div className="-my-6 divide-y divide-gray-500/10">
-                            <div className="space-y-2 py-6">
-                                {/* <Disclosure as="div" className="-mx-3">
+                </nav>
+                <Dialog open={mobileMenuOpen} onClose={setMobileMenuOpen} className="lg:hidden">
+                    <div className="fixed inset-0 z-50" />
+                    <DialogPanel className="fixed inset-y-0 right-0 z-50 w-full overflow-y-auto bg-white p-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
+                        <div className="flex items-center justify-between">
+                            <a href="#" className="-m-1.5 p-1.5">
+                                <span className="sr-only">Your Company</span>
+                                <img
+                                    alt=""
+                                    src="/logo.jpeg"
+                                    className="h-25 w-auto"
+                                />
+                            </a>
+                            <button
+                                type="button"
+                                onClick={() => setMobileMenuOpen(false)}
+                                className="-m-2.5 rounded-md p-2.5 text-gray-700"
+                            >
+                                <span className="sr-only">Close menu</span>
+                                <XMarkIcon aria-hidden="true" className="size-6" />
+                            </button>
+                        </div>
+                        <div className="mt-6 flow-root">
+                            <div className="-my-6 divide-y divide-gray-500/10">
+                                <div className="space-y-2 py-6">
+                                    {/* <Disclosure as="div" className="-mx-3">
                                     <DisclosureButton className="group flex w-full items-center justify-between rounded-lg py-2 pr-3.5 pl-3 text-base/7 font-semibold text-gray-900 hover:bg-gray-50">
                                         Home
                                         <ChevronDownIcon aria-hidden="true" className="size-5 flex-none group-data-open:rotate-180" />
@@ -188,48 +189,49 @@ export default function Example() {
                                         ))}
                                     </DisclosurePanel>
                                 </Disclosure> */}
-                                <Link href="/" className="-mx-3 block rounded-lg px-3 py-2 text-base/7 font-semibold text-gray-900 hover:bg-gray-50">
-                                    Home
-                                </Link>
-                                <Link href="/about" className="-mx-3 block rounded-lg px-3 py-2 text-base/7 font-semibold text-gray-900 hover:bg-gray-50">
-                                    About
-                                </Link>
-                                <Link href="/salering" className="-mx-3 block rounded-lg px-3 py-2 text-base/7 font-semibold text-gray-900 hover:bg-gray-50">
-                                    Sale Ring
-                                </Link>
-                                <Link href="/TermsandServices" className="-mx-3 block rounded-lg px-3 py-2 text-base/7 font-semibold text-gray-900 hover:bg-gray-50">
-                                    Terms and Conditions
-                                </Link>
-                                <Link href="/contact" className="-mx-3 block rounded-lg px-3 py-2 text-base/7 font-semibold text-gray-900 hover:bg-gray-50">
-                                    Contact
-                                </Link>
+                                    <Link href="/" className="-mx-3 block rounded-lg px-3 py-2 text-base/7 font-semibold text-gray-900 hover:bg-gray-50">
+                                        Home
+                                    </Link>
+                                    <Link href="/about" className="-mx-3 block rounded-lg px-3 py-2 text-base/7 font-semibold text-gray-900 hover:bg-gray-50">
+                                        About
+                                    </Link>
+                                    <Link href="/salering" className="-mx-3 block rounded-lg px-3 py-2 text-base/7 font-semibold text-gray-900 hover:bg-gray-50">
+                                        Sale Ring
+                                    </Link>
+                                    <Link href="/TermsandServices" className="-mx-3 block rounded-lg px-3 py-2 text-base/7 font-semibold text-gray-900 hover:bg-gray-50">
+                                        Terms and Conditions
+                                    </Link>
+                                    <Link href="/contact" className="-mx-3 block rounded-lg px-3 py-2 text-base/7 font-semibold text-gray-900 hover:bg-gray-50">
+                                        Contact
+                                    </Link>
 
-                            </div>
-                            <div className="py-6 flex gap-1">
-                                <Link href="/register" className="-mx-3 block rounded-lg px-3 py-2.5 text-base/7 font-semibold text-gray-900 hover:bg-gray-50">
-                                    <Button
-                                        variant="outline"
-                                        className="mx-1"
+                                </div>
+                                <div className="py-6 flex gap-1">
+                                    <Link href="/register" className="-mx-3 block rounded-lg px-3 py-2.5 text-base/7 font-semibold text-gray-900 hover:bg-gray-50">
+                                        <Button
+                                            variant="outline"
+                                            className="mx-1"
+                                        >
+                                            Register
+                                        </Button>
+                                    </Link>
+                                    <Link
+                                        href="/login"
+                                        className="-mx-3 block rounded-lg px-3 py-2.5 text-base/7 font-semibold text-gray-900 hover:bg-gray-50"
                                     >
-                                        Register
-                                    </Button>
-                                </Link>
-                                <Link
-                                    href="/login"
-                                    className="-mx-3 block rounded-lg px-3 py-2.5 text-base/7 font-semibold text-gray-900 hover:bg-gray-50"
-                                >
-                                    <Button
-                                        variant="default"
-                                        className="bg-[#426C6B] text-white hover:bg-[#325251]"
-                                    >
-                                        Log in
-                                    </Button>
-                                </Link>
+                                        <Button
+                                            variant="default"
+                                            className="bg-[#426C6B] text-white hover:bg-[#325251]"
+                                        >
+                                            Log in
+                                        </Button>
+                                    </Link>
+                                </div>
                             </div>
                         </div>
-                    </div>
-                </DialogPanel>
-            </Dialog>
-        </header>
+                    </DialogPanel>
+                </Dialog>
+            </header>
+        </>
     )
 }
