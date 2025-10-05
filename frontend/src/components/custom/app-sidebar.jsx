e52c12dd46b2a641e749081c5dd3920d1fe68e57
Ninetales-Publishings <147030239+saurabhkuriyal@users.noreply.github.com>
2025-10-05 23:16:32 +0530

    encapsulating appsidebar

--- frontend/src/components/custom/app-sidebar.jsx
+++ frontend/src/components/custom/app-sidebar.jsx
@@ -24,31 +24,34 @@ const items = [
     { title: "Users", url: "/admin/users", icon: Users },
     { title: "Flyer", url: "/admin/flyer", icon: FileText },
     { title: "Sale Ring", url: "/admin/search", icon: Gavel },
-    { title: "To Be Added", url: "/admin/settings", icon:   Wrench,      // for settings / to be added
- },
+    {
+        title: "To Be Added", url: "/admin/settings", icon: Wrench,      // for settings / to be added
+    },
 ];
 export function AppSidebar() {
     return (
-        <Sidebar className="">
-            <SidebarContent>
-                <SidebarGroup>
-                    <SidebarGroupLabel className="text-xl mb-2 mt-1 font-bold text-white-600">Buckers Auction</SidebarGroupLabel>
-                    <SidebarGroupContent>
-                        <SidebarMenu>
-                            {items.map((item) => (
-                                <SidebarMenuItem key={item.title}>
-                                    <SidebarMenuButton asChild>
-                                        <Link href={item.url}>
-                                            <item.icon />
-                                            <span>{item.title}</span>
-                                        </Link>
-                                    </SidebarMenuButton>
-                                </SidebarMenuItem>
-                            ))}
-                        </SidebarMenu>
-                    </SidebarGroupContent>
-                </SidebarGroup>
-            </SidebarContent>
-        </Sidebar>
+        <>
+            <Sidebar className="">
+                <SidebarContent>
+                    <SidebarGroup>
+                        <SidebarGroupLabel className="text-xl mb-2 mt-1 font-bold text-white-600">Buckers Auction</SidebarGroupLabel>
+                        <SidebarGroupContent>
+                            <SidebarMenu>
+                                {items.map((item) => (
+                                    <SidebarMenuItem key={item.title}>
+                                        <SidebarMenuButton asChild>
+                                            <Link href={item.url}>
+                                                <item.icon />
+                                                <span>{item.title}</span>
+                                            </Link>
+                                        </SidebarMenuButton>
+                                    </SidebarMenuItem>
+                                ))}
+                            </SidebarMenu>
+                        </SidebarGroupContent>
+                    </SidebarGroup>
+                </SidebarContent>
+            </Sidebar>
+        </>
     )
 }
