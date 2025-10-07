1428bf74147659a1344a0a93b352a8473fe4ec64
Ninetales-Publishings <147030239+saurabhkuriyal@users.noreply.github.com>
2025-10-07 23:26:06 +0530

    adding comments to store

--- frontend/src/lib/store.js
+++ frontend/src/lib/store.js
@@ -1,6 +1,8 @@
 import userReducer from "@/lib/features/userSlice";
 import { configureStore } from '@reduxjs/toolkit';
 
+//global store
+
 export const makeStore = () => {
     return configureStore({
         reducer: userReducer
