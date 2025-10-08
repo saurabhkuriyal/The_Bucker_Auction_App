08d03290bb2789961acc4a574d698625695a89ca
Ninetales-Publishings <147030239+saurabhkuriyal@users.noreply.github.com>
2025-10-08 23:41:35 +0530

    formatting both the models

--- backend/models/flyer.model.js
+++ backend/models/flyer.model.js
@@ -1,10 +1,15 @@
 const mongoose =require('mongoose');
 
 const flyerSchema = new mongoose.Schema({
+
     title: { type: String},
+
     description: { type: String },
+    
     imageUrl: { type: String },
-}, { timestamps: true });
+},
+    { timestamps: true });
 
 const Flyer = mongoose.model('Flyer', flyerSchema);
+
 module.exports = Flyer;
--- backend/models/user.model.js
+++ backend/models/user.model.js
@@ -2,11 +2,16 @@ const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({
     username: { type: String, required: true, unique: true },
+
     email: { type: String, required: true, unique: true },
+    
     phone: { type: String, required: true, unique: true },
+    
     password: { type: String, required: true },
+    
     role: { type: String, default: "BUYER", enum : ["BUYER", "SELLER", "ADMIN"] }
 }, { timestamps: true });
 
 const User= mongoose.model('User', userSchema);
+
 module.exports = User;
