d6015ef384e6ad8d20e101f5e99d6d789f475c06
Ninetales-Publishings <147030239+saurabhkuriyal@users.noreply.github.com>
2025-10-08 23:39:25 +0530

    formatting flyer controller

--- backend/controllers/flyer.controller.js
+++ backend/controllers/flyer.controller.js
@@ -1,25 +1,25 @@
 const cloudinary = require("cloudinary").v2;
 const Flyer = require("../models/flyer.model");
 
-async function createFlyer(req,res) {
+async function createFlyer(req, res) {
     try {
 
-        console.log("reached here in flyer controller",req.body);
-        console.log("file details",req.file);
+        console.log("reached here in flyer controller", req.body);
+        console.log("file details", req.file);
 
         //for storing image in cloudinary
         flyerImage = "";
         if (req.file) {
             cloudinary.config({
                 cloud_name: "deuofkrkf",
-                api_key:process.env.CLOUDINARY_API_KEY,
+                api_key: process.env.CLOUDINARY_API_KEY,
                 api_secret: process.env.CLOUNDINARY_API_SECRET
             });
 
             const result = await cloudinary.uploader.upload(req.file.path);
             //console.log(result.secure_url, 'uploaded.secure_url');
             flyerImage = result.secure_url;
-        }        
+        }
 
         const { title, description, } = req.body;
 
@@ -32,14 +32,14 @@ async function createFlyer(req,res) {
         await newFlyer.save();
 
         //console.log("new flyer details----",newFlyer);
-        
-        return res.status(200).json({success:true,message: "Flyer created successfully"});
-        
-        
+
+        return res.status(200).json({ success: true, message: "Flyer created successfully" });
+
+
     } catch (error) {
         //console.log("flyer creation error",error );
-        
-        return res.status(500).json({success:false, message: "Internal Server Error"});
+
+        return res.status(500).json({ success: false, message: "Internal Server Error" });
     }
 }
 
@@ -48,14 +48,15 @@ async function getFlyers(req, res) {
     try {
         const flyers = await Flyer.findOne().sort({ createdAt: -1 });
 
-       // console.log("Fetched flyers:", flyers);
-        
+        // console.log("Fetched flyers:", flyers);
+
         return res.status(200).json({ success: true, flyers });
-    }   catch (error) {
+    } catch (error) {
         console.error("Error fetching flyers:", error);
         return res.status(500).json({ success: false, message: "Internal Server Error" });
     }
 }
-module.exports = { createFlyer,
+module.exports = {
+    createFlyer,
     getFlyers,
- };
+};
