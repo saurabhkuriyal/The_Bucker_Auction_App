e051cc2160830663ebc5f92a5094bd93144ab1f1
Ninetales-Publishings <147030239+saurabhkuriyal@users.noreply.github.com>
2025-10-10 23:31:18 +0530

    addimg comment to register route

--- backend/routes/user.route.js
+++ backend/routes/user.route.js
@@ -6,10 +6,9 @@ const userController = require('../controllers/user.controller');
 router.use(bodyParser.json());
 // eoute for login
 router.route("/register").post(userController.registerUser);
-
+//route for registration
 router.route("/login").post(userController.loginUser);
 
 router.route("/getAllUser").get(userController.getAllUser);
-
 
 module.exports = router;
